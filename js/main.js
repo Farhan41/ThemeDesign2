cf800f5537f023025450b686c6744cefb4cb5e63
Farhan Ferdous <farhanferdous@gmail.com>
2023-03-24 01:19:19 +0600

    loader

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 // alert('Assalamu Alaikum');
 
-// navbar toggler icon
+(function() {
+
+  'use strict';
+
+  // preloader
+  $(window).on('load', function() {
+      $('.loader').fadeOut('slow');
+  });
+
+
+  // navbar toggler icon
 $(document).on("click",".navbar-toggler",function(e){
-    $(this).parent().siblings().find('i').removeClass('fa-x')
-  $(this).find('i').toggleClass('fa-x')
+  $(this).parent().siblings().find('i').removeClass('fa-x')
+$(this).find('i').toggleClass('fa-x')
 });
+
+})();
+  
